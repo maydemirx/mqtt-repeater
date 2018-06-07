62f163f7fb3893eabbd0f9dc3d5427211b40ae7b
Mehmet Aydemir <maydemirx@gmail.com>
2018-06-07 10:06:31 +0300

    Create index.js

---
+++ test/index.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+const should = require('chai').should();
+
+
+describe('Repeater#isConnected()', function() {
+    it('should return false when the connect not called yet', function() {
+        assert.equal(false, false);
+    });
+});
